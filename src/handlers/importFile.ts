e931a7caf29ec22e1884e151f2f31be9f84e110c
viatrix <viatrix.x@gmail.com>
2020-04-02 14:39:01 +0300

    Changed eslint rule "semi" to "always"

--- src/handlers/importFile.ts
+++ src/handlers/importFile.ts
@@ -1,53 +1,53 @@
-import { S3Event, S3Handler } from 'aws-lambda'
-import { S3 } from 'aws-sdk'
-import 'source-map-support/register'
-import * as config from '../../config.json'
-import * as awsConfig from '../../awsConfig.json'
-import { ImportFileService } from '../services/importFileService'
+import { S3Event, S3Handler } from 'aws-lambda';
+import { S3 } from 'aws-sdk';
+import 'source-map-support/register';
+import * as config from '../../config.json';
+import * as awsConfig from '../../awsConfig.json';
+import { ImportFileService } from '../services/importFileService';
 
 export const validateEvent = (event: S3Event): boolean => {
   if (!event.Records || event.Records.length === 0) {
-    console.error('Error! No files are uploaded')
-    return false
+    console.error('Error! No files are uploaded');
+    return false;
   }
   if (event.Records.length > 1) {
-    console.error('Error! More than one file is uploaded')
-    return false
+    console.error('Error! More than one file is uploaded');
+    return false;
   }
-  return true
-}
+  return true;
+};
 
 export const getFileContents = async (key: string): Promise<string> => {
   const s3 = new S3({
     region: awsConfig.region,
     accessKeyId: awsConfig.accessKeyId,
     secretAccessKey: awsConfig.secretAccessKey
-  })
+  });
   const result = await s3.getObject({
     Bucket: config.s3.bucket,
     Key: key
-  }).promise()
-  const contents = result.Body
+  }).promise();
+  const contents = result.Body;
   if (!contents) {
-    throw new Error('File contents was not retrieved')
+    throw new Error('File contents was not retrieved');
   }
-  return contents.toString()
-}
+  return contents.toString();
+};
 
 export const importFile: S3Handler = async (event) => {
   if (!validateEvent(event)) {
-    return
+    return;
   }
-  const { key } = event.Records[0].s3.object
-  console.info(`File import is triggered for the file ${key}`)
+  const { key } = event.Records[0].s3.object;
+  console.info(`File import is triggered for the file ${key}`);
 
-  const data = await getFileContents(key)
-  console.info('Retrieved file from S3')
-  const importFileService = new ImportFileService()
-  await importFileService.init()
-  console.info('Connected to DB')
-  await importFileService.clearDb()
-  console.info('Cleared DB')
-  await importFileService.parseFile(data)
-  console.info('Import was finished successfully')
-}
+  const data = await getFileContents(key);
+  console.info('Retrieved file from S3');
+  const importFileService = new ImportFileService();
+  await importFileService.init();
+  console.info('Connected to DB');
+  await importFileService.clearDb();
+  console.info('Cleared DB');
+  await importFileService.parseFile(data);
+  console.info('Import was finished successfully');
+};
--- src/services/importFileService.ts
+++ src/services/importFileService.ts
@@ -5,17 +5,17 @@ import {
   getConnection,
   DatabaseType,
   Repository
-} from 'typeorm'
-import 'reflect-metadata'
-import { AccessTypes, ApplTypes, Product } from '../db/entity/products'
-import { Applicant } from '../db/entity/applicants'
-import { DosageForm } from '../db/entity/dosageForms'
-import { Ingredient } from '../db/entity/ingredients'
-import { ProductIngredient } from '../db/entity/productIngredients'
+} from 'typeorm';
+import 'reflect-metadata';
+import { AccessTypes, ApplTypes, Product } from '../db/entity/products';
+import { Applicant } from '../db/entity/applicants';
+import { DosageForm } from '../db/entity/dosageForms';
+import { Ingredient } from '../db/entity/ingredients';
+import { ProductIngredient } from '../db/entity/productIngredients';
 
-import { Route } from '../db/entity/routes'
-import * as config from '../../config.json'
-import 'pg'
+import { Route } from '../db/entity/routes';
+import * as config from '../../config.json';
+import 'pg';
 
 export type ProductType = {
   ingredients: string[];
@@ -63,93 +63,93 @@ export class ImportFileService {
       entities: config.typeorm.entities,
       migrations: config.typeorm.migrations,
       subscribers: config.typeorm.subscribers
-    }
+    };
     if (!this.connection) {
       try {
-        this.connection = getConnection()
+        this.connection = getConnection();
       } catch (err) {
         if (err.name !== 'ConnectionNotFoundError') {
-          throw err
+          throw err;
         }
       }
     }
     if (!this.connection) {
-      this.connection = await createConnection(connectionOptions as ConnectionOptions)
+      this.connection = await createConnection(connectionOptions as ConnectionOptions);
     }
-    this.applicantsRepository = this.connection.getRepository(Applicant)
-    this.dosageFormsRepository = this.connection.getRepository(DosageForm)
-    this.ingredientsRepository = this.connection.getRepository(Ingredient)
-    this.productIngredientsRepository = this.connection.getRepository(ProductIngredient)
-    this.productsRepository = this.connection.getRepository(Product)
-    this.routesRepository = this.connection.getRepository(Route)
+    this.applicantsRepository = this.connection.getRepository(Applicant);
+    this.dosageFormsRepository = this.connection.getRepository(DosageForm);
+    this.ingredientsRepository = this.connection.getRepository(Ingredient);
+    this.productIngredientsRepository = this.connection.getRepository(ProductIngredient);
+    this.productsRepository = this.connection.getRepository(Product);
+    this.routesRepository = this.connection.getRepository(Route);
   }
 
   public async clearDb () {
-    await this.productIngredientsRepository.delete({})
-    await this.productsRepository.delete({})
-    await this.applicantsRepository.delete({})
-    await this.dosageFormsRepository.delete({})
-    await this.ingredientsRepository.delete({})
-    await this.routesRepository.delete({})
+    await this.productIngredientsRepository.delete({});
+    await this.productsRepository.delete({});
+    await this.applicantsRepository.delete({});
+    await this.dosageFormsRepository.delete({});
+    await this.ingredientsRepository.delete({});
+    await this.routesRepository.delete({});
   }
 
   public async parseFile (data: string) {
-    const dataWithoutHeaders = data.slice(data.indexOf('\n') + 1)
-    const lines = dataWithoutHeaders.split('\n')
+    const dataWithoutHeaders = data.slice(data.indexOf('\n') + 1);
+    const lines = dataWithoutHeaders.split('\n');
     for (const line of lines) {
-      const trimmedLine = line.replace('\n', '')
+      const trimmedLine = line.replace('\n', '');
       if (trimmedLine.length) {
-        const product = this.parseLine(trimmedLine)
-        await this.saveToDb(product)
+        const product = this.parseLine(trimmedLine);
+        await this.saveToDb(product);
       }
     }
   }
 
   public parseLine (line: string): ProductType {
-    const lineParts = line.split('~')
+    const lineParts = line.split('~');
     if (lineParts.length !== 14) {
-      throw new Error(`Line doesn't contain 14 columns separated by ~: ${line}`)
+      throw new Error(`Line doesn't contain 14 columns separated by ~: ${line}`);
     }
-    const ingredients = lineParts[0].split('; ')
-    const dosageForm = lineParts[1].slice(0, lineParts[1].indexOf(';'))
-    const route = lineParts[1].slice(lineParts[1].indexOf(';') + 1)
-    const tradeName = lineParts[2]
-    const applicant = lineParts[3]
-    const strength = lineParts[4].split(';')
-    const applTypeString = lineParts[5]
-    let applType: ApplTypes
+    const ingredients = lineParts[0].split('; ');
+    const dosageForm = lineParts[1].slice(0, lineParts[1].indexOf(';'));
+    const route = lineParts[1].slice(lineParts[1].indexOf(';') + 1);
+    const tradeName = lineParts[2];
+    const applicant = lineParts[3];
+    const strength = lineParts[4].split(';');
+    const applTypeString = lineParts[5];
+    let applType: ApplTypes;
     switch (applTypeString) {
       case 'N':
-        applType = ApplTypes.ORIGINAL
-        break
+        applType = ApplTypes.ORIGINAL;
+        break;
       case 'A':
-        applType = ApplTypes.GENERIC
-        break
+        applType = ApplTypes.GENERIC;
+        break;
       default:
-        throw new Error(`Invalid appl_type ${applTypeString}`)
+        throw new Error(`Invalid appl_type ${applTypeString}`);
     }
-    const applNo = +lineParts[6]
-    const productNo = +lineParts[7]
-    const teCode = lineParts[8]
-    const approvalDate = new Date(lineParts[9])
-    const rld = lineParts[10] === 'Yes'
-    const rs = lineParts[11] === 'Yes'
-    const accessTypeString = lineParts[12]
-    let accessType: AccessTypes
+    const applNo = +lineParts[6];
+    const productNo = +lineParts[7];
+    const teCode = lineParts[8];
+    const approvalDate = new Date(lineParts[9]);
+    const rld = lineParts[10] === 'Yes';
+    const rs = lineParts[11] === 'Yes';
+    const accessTypeString = lineParts[12];
+    let accessType: AccessTypes;
     switch (accessTypeString) {
       case 'RX':
-        accessType = AccessTypes.RX
-        break
+        accessType = AccessTypes.RX;
+        break;
       case 'OTC':
-        accessType = AccessTypes.OTC
-        break
+        accessType = AccessTypes.OTC;
+        break;
       case 'DISCN':
-        accessType = AccessTypes.DISCN
-        break
+        accessType = AccessTypes.DISCN;
+        break;
       default:
-        throw new Error(`Invalid access_type ${accessTypeString}`)
+        throw new Error(`Invalid access_type ${accessTypeString}`);
     }
-    const applicantFullName = lineParts[13]
+    const applicantFullName = lineParts[13];
     return {
       ingredients,
       dosageForm,
@@ -166,31 +166,31 @@ export class ImportFileService {
       rs,
       accessType,
       applicantFullName
-    }
+    };
   }
 
   public async saveToDb (product: ProductType) {
     if (product.ingredients.length !== product.strength.length) {
-      throw new Error(`Incorrect list of ingredients and strength for ${product.tradeName}`)
+      throw new Error(`Incorrect list of ingredients and strength for ${product.tradeName}`);
     }
-    let applicantEntry = await this.applicantsRepository.findOne(product.applicant)
+    let applicantEntry = await this.applicantsRepository.findOne(product.applicant);
     if (!applicantEntry) {
       applicantEntry = await this.applicantsRepository.save({
         applicantShortName: product.applicant,
         applicantFullName: product.applicantFullName
-      })
+      });
     }
-    let dosageFormEntry = await this.dosageFormsRepository.findOne({ where: { dosageForm: product.dosageForm } })
+    let dosageFormEntry = await this.dosageFormsRepository.findOne({ where: { dosageForm: product.dosageForm } });
     if (!dosageFormEntry) {
       dosageFormEntry = await this.dosageFormsRepository.save({
         dosageForm: product.dosageForm
-      })
+      });
     }
-    let routeEntry = await this.routesRepository.findOne({ where: { route: product.route } })
+    let routeEntry = await this.routesRepository.findOne({ where: { route: product.route } });
     if (!routeEntry) {
       routeEntry = await this.routesRepository.save({
         route: product.route
-      })
+      });
     }
     const productEntry = await this.productsRepository.save({
       tradeName: product.tradeName,
@@ -205,17 +205,17 @@ export class ImportFileService {
       rld: product.rld,
       rs: product.rs,
       accessType: product.accessType
-    })
+    });
     for (let i = 0; i < product.ingredients.length; i++) {
-      let ingredientEntry = await this.ingredientsRepository.findOne({ where: { ingredient: product.ingredients[i] } })
+      let ingredientEntry = await this.ingredientsRepository.findOne({ where: { ingredient: product.ingredients[i] } });
       if (!ingredientEntry) {
-        ingredientEntry = await this.ingredientsRepository.save({ ingredient: product.ingredients[i] })
+        ingredientEntry = await this.ingredientsRepository.save({ ingredient: product.ingredients[i] });
       }
       await this.productIngredientsRepository.save({
         product: productEntry,
         ingredient: ingredientEntry,
         strength: product.strength[i]
-      })
+      });
     }
   }
 }
